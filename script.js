eededb7a6424be3ee4dbc47728f7e166fc65fed5
alcidys <alcidysapb@al.insper.edu.br>
2026-06-16 23:25:57 -0300

    até ione

--- script.js
+++ script.js
@@ -7,21 +7,106 @@ const mensagens = {
 • Luke Flier — Instagram: @luke_fl1912 | WhatsApp: +31 6 83776041
 • Samuele Scalisi — Instagram: @samuele_scalisi | WhatsApp: +39 389 480 4712`,
 
-    "alvaromrbs": "Olá, Alvaro! Este é o seu grupo de intercambistas:",
-    "analajdf": "Olá, Ana! Este é o seu grupo de intercambistas:",
-    "analcc1": "Olá, Ana Luiza! Este é o seu grupo de intercambistas:",
-    "beatrizdh": "Olá, Beatriz! Este é o seu grupo de intercambistas:",
-    "caiohn": "Olá, Caio! Este é o seu grupo de intercambistas:",
-    "carolinaor": "Olá, Carolina! Este é o seu grupo de intercambistas:",
-    "celinespl": "Olá, Celine! Este é o seu grupo de intercambistas:",
-    "enzouc": "Olá, Enzo! Este é o seu grupo de intercambistas:",
-    "felipecm13": "Olá, Felipe! Este é o seu grupo de intercambistas:",
-    "felipeqs": "Olá, Felipe! Este é o seu grupo de intercambistas:",
-    "fellipebvs": "Olá, Fellipe! Este é o seu grupo de intercambistas:",
-    "gabrielads1": "Olá, Gabriela! Este é o seu grupo de intercambistas:",
-    "guilhermenb": "Olá, Guilherme! Este é o seu grupo de intercambistas:",
-    "iaravsp": "Olá, Iara! Este é o seu grupo de intercambistas:",
-    "ionef": "Olá, Ione! Este é o seu grupo de intercambistas:",
+   
+    "alvaromrbs": `Olá, Alvaro! Este é o seu grupo de intercambistas:
+
+• Finn Jacob Westphal — Instagram: @finn.wspl | WhatsApp: +49 1575 0639268
+• Matteo Leandro Lenti — Instagram: @matteo.le._
+• Noé Combettes — Instagram: @noe.cbts | WhatsApp: +33 662157452
+• Xavier Yamato Nishikawa — Instagram: @Xavierynx | WhatsApp: +1 2069994495`,
+
+    "analajdf": `Olá, Ana! Este é o seu grupo de intercambistas:
+
+• Flavie Océane dos Santos — Instagram: flv.dst
+• Sofie Geipel — WhatsApp: +351936493266 | Instagram: sofiegeipeldk
+• Chanel Rose Mc Cormick — Instagram: _chanelogy`,
+
+    "analcc1": `Olá, Ana Luiza! Este é o seu grupo de intercambistas:
+
+• Carla Brdenk — WhatsApp: +33648506669 | Instagram: carla_brdk
+• Margot Tosti Pautel — WhatsApp: +34 644764387 | Instagram: margot.tsp
+• Josie Mariama Reiss — Instagram: reissjosie@gmail.com
+• Eline Lureaux — WhatsApp: +33767084171 | Instagram: eline.lrx`,
+
+    "beatrizdh": `Olá, Beatriz! Este é o seu grupo de intercambistas:
+
+• Maria Mariano Pêgo Chieira — WhatsApp: +351 963887515 | Instagram: mariamarianopego
+• Leonor de Ascensão Fonseca Carreira — WhatsApp: +351 935618257 | Instagram: leonorcarreiraa
+• Anna Aude Elise Le Dréan — WhatsApp: +33769417720 | Instagram: _anna_ld`,
+
+    "caiohn": `Olá, Caio! Este é o seu grupo de intercambistas:
+
+• Alejandro Martínez Ferreira — Instagram: @aleex.mtneez_ | WhatsApp: +34 626479144
+• Leo Nicolas Lappalainen — Instagram: @leo_lapp | WhatsApp: +352 661 921 104
+• Mateo Preve — Instagram: @matepreve | WhatsApp: +39 3755237184
+• Sam Langlet — Instagram: @sam_langlet | WhatsApp: +32 499602349`,
+
+    "carolinaor": `Olá, Carolina! Este é o seu grupo de intercambistas:
+
+• Carlota Bárbara Tor — WhatsApp: +34 699528195 | Instagram: carlotabarbara
+• Sarah Sofía Ochoa Martínez — WhatsApp: +57 3133982855 | Instagram: sarahochoam
+• Solange Marie-Josèphe Marwa Beaumier — WhatsApp: +33638634880 | Instagram: so.bmh`,
+
+    "celinespl": `Olá, Celine! Este é o seu grupo de intercambistas:
+
+• Nicole Mammoliti — WhatsApp: +393516812343 | Instagram: Nicole_mammo
+• Lavinia Toraldo — WhatsApp: +393494092052 | Instagram: Laviniaatoraldo
+• Pénélope Le Bras — WhatsApp: 0642643726 | Instagram: penelopelbs`,
+
+    "enzouc": `Olá, Enzo! Este é o seu grupo de intercambistas:
+
+• Enrique Rivela Gómez — WhatsApp: +34 682841317
+• Joaquin Peñaloza — Instagram: @joaquin_penaloza5
+• Jorge Corugedo Alonso — WhatsApp: +34 717712057
+• Juan Manuel Castellanos Roldán — Instagram: @_juan_m10 | WhatsApp: +57 3233267063`,
+
+    "felipecm13": `Olá, Felipe! Este é o seu grupo de intercambistas:
+
+• Aleksi Paul Lappalainen — Instagram: @aleksi_lapp | WhatsApp: +33 7 44 85 68 12
+• Ben Georg Wolf Beltermann — Instagram: @Ben.beltermann | WhatsApp: +49 15116009483
+• Gyan Teejay Brian Jansen — Instagram: @Gyan.jansen | WhatsApp: +31 6 15152893
+• Léon Patrick Titus De Baets — Instagram: @leondebaets | WhatsApp: +32 471 06 90 09`,
+
+    "felipeqs": `Olá, Felipe! Este é o seu grupo de intercambistas:
+
+• Boldizsar Hajdu — WhatsApp: +36 301568961
+• Lorenzo Ioppoli — Instagram: @lorenzo_ioppoli_ | WhatsApp: +39 3889298652
+• Mees Ole Schat — Instagram: @meesschat | WhatsApp: +31 6 40672992
+• Wilhelm Schubert — Instagram: @Wilhelm_schubert | WhatsApp: +46 723877650`,
+
+    "fellipebvs": `Olá, Fellipe! Este é o seu grupo de intercambistas:
+
+• Adrian Primorac — Instagram: @primo.jpeg | WhatsApp: +47 91916968
+• Joan Casals Orriols — Instagram: @joancasals | WhatsApp: +34 644740807
+• Mattéo Duval — Instagram: @matt_duvallll | WhatsApp: +33 7 71 60 71 37
+• Thomas Alexis Marc Marie de Montille — Instagram: @thomas.dmtl | WhatsApp: +33 782835501`,
+
+    "gabrielads1": `Olá, Gabriela! Este é o seu grupo de intercambistas:
+
+• Anne Corien Alice Koomans — WhatsApp: +31 6 31 95 20 31 | Instagram: annekoomans
+• Eva Esther Marie Harson — WhatsApp: +33610843619 | Instagram: evaaa_hrs
+• Svenja Bayreuther — Instagram: Svenja.454`,
+
+    "guilhermenb": `Olá, Guilherme! Este é o seu grupo de intercambistas:
+
+• Bhuvan Kasam — WhatsApp: +91 9550862525
+• Geo Cauzza — Instagram: @geo.cauzza | WhatsApp: +41 764430113
+• Gustavo Coutada Pinto Mateus — Instagram: @Gustavo_coutada
+• Sven Mollo — Instagram: @sven_mll | WhatsApp: +32 471 55 79 87`,
+
+    "iaravsp": `Olá, Iara! Este é o seu grupo de intercambistas:
+
+• Annelyse Suzanne Lelasseux — WhatsApp: +33781782956 | Instagram: A.nnelyse.l
+• Helena Kiil — WhatsApp: +47 46828840 | Instagram: Helena.kiil
+• Margarida Freire Barbas de Albuquerque — WhatsApp: +351 917200413 | Instagram: margarida.albuquerquee`,
+
+    "ionef": `Olá, Ione! Este é o seu grupo de intercambistas:
+
+• Camille Odile Francine Defert Bentley — WhatsApp: 0782480719 | Instagram: cam_btly
+• Maja Natalia Stasiewicz — WhatsApp: +48 732 761 307 | Instagram: maja.stasiewicz
+• Anetta Tángli — WhatsApp: +36203752810 | Instagram: t_anetta_
+• Laeticia Elisa Eugenia Cristina Perrin — WhatsApp: +44 7833 555177 | Instagram: laeticia_perrin`,
+
     "jesuinomc": "Olá, Jesuino! Este é o seu grupo de intercambistas:",
     "joaofmt": "Olá, João! Este é o seu grupo de intercambistas:",
     "juliabp2": "Olá, Julia! Este é o seu grupo de intercambistas:",
